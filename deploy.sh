2e9e99da6a893c0fda79c7c52ff32ac01612f316
Ian Edwards <denkyuu@gmail.com>
2021-01-27 15:52:01 -0500

    Fix gulp and add stopgap deployment method.

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+# Stop-gap deployment schema. 
+
+# html
+az storage blob upload-batch --source . --pattern "*.html" --account-name neonav2webchat --destination '$web' --destination-path .
+# assets
+az storage blob upload-batch --source ./assets --account-name neonav2webchat --destination '$web' --destination-path ./assets
+# libs
+az storage blob upload-batch --source ./libs --account-name neonav2webchat --destination '$web' --destination-path ./libs
