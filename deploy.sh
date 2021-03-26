8d38cc8e9bae4baaad9696c54e0e7c9e50845fe9
Ian Edwards <denkyuu@gmail.com>
2021-03-25 22:03:22 -0400

    Add output location to github action.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-# Stop-gap deployment schema. 
+# Stop-gap deployment schema by Ian. 
 
 # html
 az storage blob upload-batch --source . --pattern "*.html" --account-name neonav2webchat --destination '$web' --destination-path .
@@ -6,3 +6,5 @@ az storage blob upload-batch --source . --pattern "*.html" --account-name neonav
 az storage blob upload-batch --source ./assets --account-name neonav2webchat --destination '$web' --destination-path ./assets
 # libs
 az storage blob upload-batch --source ./libs --account-name neonav2webchat --destination '$web' --destination-path ./libs
+# favicons
+az storage blob upload-batch --source . --pattern "*.ico" --account-name neonav2webchat --destination '$web' --destination-path .
